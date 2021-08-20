6c1f8e315a89af523871f1685752604e7a5c849c
Pavel Kondrashev <kondrashev@ukr.net>
2021-08-20 19:46:54 +0300

    'dictionary_develop_12'

--- src/main/js/components/admin/TableUsers.js
+++ src/main/js/components/admin/TableUsers.js
@@ -36,7 +36,7 @@ function UsersTable(props) {
             setValues: setValues
         }
         getUsers(data);
-    }, [updateUsers]);
+    }, [updateUsers, values.listUsers]);
     function descendingComparator(a, b, orderBy) {
         if (b[orderBy] < a[orderBy]) {
             return -1;
